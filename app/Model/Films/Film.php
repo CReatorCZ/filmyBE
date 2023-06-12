1ebb194a89e5481f025e07f1edca220ba669f674
Jakub <kozak.jakub@post.cz>
2023-06-12 14:49:23 +0200

    vizuální upravení kodu + priprava na herce

--- app/Model/Films/Film.php
+++ app/Model/Films/Film.php
@@ -2,20 +2,23 @@
 
 namespace App\Model\Films;
 
+use App\Model\Actors\Actor;
 use Nextras\Orm\Entity\Entity;
+use Nextras\Orm\Relationships\ManyHasMany;
 
 /**
  * Film
  *
- * @property int        $id    {primary}
- * @property string     $name
- * @property string     $genre
- * @property string     $direction
- * @property int        $releaseDate
- * @property int        $length
- * @property int        $rating
- * @property string     $nationalOrigin
- * @property string     $description
+ * @property int                    $id             {primary}
+ * @property string                 $name
+ * @property string                 $genre
+ * @property string                 $direction
+ * @property int                    $releaseDate
+ * @property int                    $length
+ * @property int                    $rating
+ * @property string                 $nationalOrigin
+ * @property string                 $description
+ * @property ManyHasMany|Actor[]    $actors         {m:m Actor::$films, isMain=true}
  */
 class Film extends Entity
 {
